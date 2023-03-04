8ad130215b2ff21d4acc7481d4ee7d614c9c3cc3
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2023-03-04 19:52:21 +0100

    selection menu but singlePlayer only

---
+++ My project/Assets/Scripts/SelectionScripts/PlayerSpawner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    void Start()
+    {
+        Instantiate(GameManager.instance.currentCharacter.prefab, transform.position, Quaternion.identity);
+    }
+}
