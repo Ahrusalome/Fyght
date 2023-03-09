616227cdd7580d65deffc8adb67a88fdc67734f8
Ahrusalome <90481871+Ahrusalome@users.noreply.github.com>
2023-03-09 15:11:58 +0100

    screen selection more UI

--- My project/Assets/Scripts/SelectionScripts/CharacterSelectionUI.cs
+++ My project/Assets/Scripts/SelectionScripts/CharacterSelectionUI.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class CharacterSelectionUI : MonoBehaviour
 {
    public GameObject optionPrefab;
    public Transform prevCharacter;
    public Transform selectedCharacter;
+   public GameObject textIndicator;
 
    private void Start() {
+      textIndicator = GameObject.Find("Indicator");
       foreach (Character c in GameManager.instance.characters)  {
         GameObject option = Instantiate(optionPrefab, transform);
         Button button = option.GetComponent<Button>();
@@ -30,6 +33,12 @@ public class CharacterSelectionUI : MonoBehaviour
         }
    }
        void Update() {
+         if (GameManager.instance.currentCharacterP1.name != "") {
+            textIndicator.GetComponent<Text>().text = "Player 2 choose his fighter !";
+         }
+         if (GameManager.instance.currentCharacterP2.name != "") {
+            textIndicator.GetComponent<Text>().text = "Click the button to launch the game !";
+         }
          if (selectedCharacter != null) {
             selectedCharacter.localScale = Vector3.Lerp(selectedCharacter.localScale, new Vector3(1.2f, 1.2f, 1.2f), Time.deltaTime * 10);
          }
