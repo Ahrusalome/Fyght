1bbd63d11a09779a61a1dfe07877f523f2c97a8b
Lisachard <lisa.achard@ynov.com>
2023-03-24 15:38:15 +0100

    MAJOR PUSH : remplacing misfunctionning menu, adding shades (opacity) and fixing major bugs

--- My project/Assets/Scripts/ButtonAction/PauseMenu.cs
+++ My project/Assets/Scripts/ButtonAction/PauseMenu.cs
@@ -3,6 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MainMenu
 {
+    public bool isPausedMenu = false;
+    public GameObject pauseMenuUI;
+
+    void Start() {
+        pauseMenuUI.SetActive(isPausedMenu);    
+    }
+
+    void OnEscape() {  
+        Debug.Log("Escaping game succeeded");
+        if (!isPausedMenu) {
+            PauseGame();
+        } else {
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame() {
+        pauseMenuUI.SetActive(true);    
+        Time.timeScale = 0f;
+        isPausedMenu = !isPausedMenu; 
+        Debug.Log("Pausing game succeeded");
+    }
+
+    public void ResumeGame() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPausedMenu = !isPausedMenu;
+        Debug.Log("Resuming game succeeded");
+    }
+
+    public void LoadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu"); 
+        Debug.Log("Loading menu succeeded");
+    }
 }
