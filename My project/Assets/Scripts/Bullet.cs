cb506c354bf9e32a299d580b770c24c23a61051d
AROLLAND1 <alexandre.rolland85@ynov.com>
2023-03-27 16:00:08 +0200

    FIX : some merge problem

--- My project/Assets/Scripts/Bullet.cs
+++ My project/Assets/Scripts/Bullet.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -20,7 +18,7 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject);
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (transform.position.x < -20)
         {
@@ -31,6 +29,4 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
 }
